c1c718e51897f11b15ba4b56f5a7b2c4fc6e3abb
Jim Coffey <james.coffey@digital.hmrc.gov.uk>
2017-01-19 12:01:32 +0000

    Add private jenkins test script

---
+++ jenkins-start-with-assets-frontend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Starting ASSETS"
+
+cd $WORKSPACE
+rm -rf service-manager-config
+git clone ***REMOVED***
+
+sm --stop ALL
+sm --cleanlogs
+sm --start ASSETS_FRONTEND -r --wait 60 --noprogress
+
+echo "Running tests for Api Documentation Frontend"
+
+cd $WORKSPACE
+
+echo "Start tests..."
+sbt clean test acceptance:test
+
+echo "Publish..."
+sbt dist-tgz publish
+
+echo "Gracefully shutdown server..."
+
+sm --stop ALL
