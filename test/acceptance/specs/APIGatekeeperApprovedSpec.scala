b5839120b4575da161c4c01f7440c60b79c111bc
Purnima Shanti <purnimashanti26@gmail.com>
2017-02-01 09:59:17 +0000

    Strategic sandbox tests (#82)
    * Added SandboxTest and NonSandboxTest tags, added SandboxBaseSpec class, added snadbox config in MicroService class, Added StrategicSandbox scenarios
    
    * Commented out the assertions which uses assets
    
    * Added run_acceptance_test.sh file

--- test/acceptance/specs/APIGatekeeperApprovedSpec.scala
+++ test/acceptance/specs/APIGatekeeperApprovedSpec.scala
@@ -22,13 +22,13 @@ import acceptance.pages.{ApprovedPage, DashboardPage, ResendVerificationPage}
 import acceptance.{BaseSpec, SignInSugar}
 import com.github.tomakehurst.wiremock.client.WireMock._
 import component.matchers.CustomMatchers
-import org.scalatest.Matchers
+import org.scalatest.{Matchers, Tag}
 
 class APIGatekeeperApprovedSpec extends BaseSpec with SignInSugar with Matchers with CustomMatchers with MockDataSugar {
 
   feature("View approved application details") {
 
-    scenario("View details for an application in production") {
+    scenario("View details for an application in production", Tag("NonSandboxTest")) {
 
       stubApplicationListAndDevelopers
       stubFor(get(urlEqualTo(s"/gatekeeper/application/$approvedApp1"))
@@ -46,7 +46,7 @@ class APIGatekeeperApprovedSpec extends BaseSpec with SignInSugar with Matchers 
       assertApplicationDetails
      }
 
-    scenario("View details for an application pending verification") {
+    scenario("View details for an application pending verification", Tag("NonSandboxTest")) {
 
       stubApplicationListAndDevelopers
       stubFor(get(urlEqualTo(s"/gatekeeper/application/$approvedApp1"))
@@ -72,7 +72,7 @@ class APIGatekeeperApprovedSpec extends BaseSpec with SignInSugar with Matchers 
       assertApplicationDetails
     }
 
-    scenario("View details for an application with no description"){
+    scenario("View details for an application with no description", Tag("NonSandboxTest")){
 
       stubApplicationListAndDevelopers
       stubFor(get(urlEqualTo(s"/gatekeeper/application/$approvedApp1"))
@@ -87,7 +87,7 @@ class APIGatekeeperApprovedSpec extends BaseSpec with SignInSugar with Matchers 
       assertApplicationDetails
     }
 
-    scenario("Navigate back to the dashboard page") {
+    scenario("Navigate back to the dashboard page", Tag("NonSandboxTest")) {
       stubApplicationListAndDevelopers
       stubFor(get(urlEqualTo(s"/gatekeeper/application/$approvedApp1"))
         .willReturn(aResponse().withBody(approvedApplication()).withStatus(200)))
---
+++ test/acceptance/specs/APIGatekeeperSandboxSpec.scala
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package acceptance.specs
+
+import acceptance.pages.{ApplicationPage, SignInPage}
+import acceptance.{SandboxBaseSpec, SignInSugar}
+import com.github.tomakehurst.wiremock.client.WireMock._
+import component.matchers.CustomMatchers
+import org.openqa.selenium.By
+import org.scalatest.{GivenWhenThen, Matchers}
+import org.scalatest.Tag
+
+import scala.io.Source
+
+class APIGatekeeperSandboxSpec extends SandboxBaseSpec with SignInSugar with Matchers with CustomMatchers with MockDataSugar with GivenWhenThen {
+
+  feature("Strategic Sandbox - Gatekeeper - understand I am on the Enhanced Test Service") {
+
+    info("As an admin (SDST)")
+    info("I can tell that I am on the Enhanced Test Service version of Gatekeeper")
+    info("So that I don't get confused about where I am")
+
+    scenario("Ensure developer is on Gatekeeper in ET and they know it", Tag("SandboxTest")) {
+
+      stubApplicationList()
+      stubFor(get(urlEqualTo(s"/gatekeeper/application/$approvedApp1"))
+        .willReturn(aResponse().withBody(approvedApplication("application description", true)).withStatus(200)))
+      val applicationsList = Source.fromURL(getClass.getResource("/applications.json")).mkString.replaceAll("\n", "")
+
+      stubFor(get(urlEqualTo(s"/application")).willReturn(aResponse()
+        .withBody(applicationsList).withStatus(200)))
+      stubApplicationSubscription
+      stubApiDefinition
+
+      val authBody =
+        s"""
+           |{
+           | "access_token": {
+           |     "authToken":"Bearer fggjmiJzyVZrR6/e39TimjqHyla3x8kmlTd",
+           |     "expiry":1459365831061
+           |     },
+           |     "expires_in":14400,
+           |     "roles":[{"scope":"api","name":"gatekeeper"}],
+           |     "authority_uri":"/auth/oid/$gatekeeperId",
+           |     "token_type":"Bearer"
+           |}
+      """.stripMargin
+
+      stubFor(post(urlEqualTo("/auth/authenticate/user"))
+        .willReturn(aResponse().withBody(authBody).withStatus(200)))
+
+      stubFor(get(urlEqualTo("/auth/authenticate/user/authorise?scope=api&role=gatekeeper"))
+        .willReturn(aResponse().withStatus(200)))
+
+      Given("the developer goes to the Gatekeeper home page")
+      goOn(SignInPage)
+      on(SignInPage)
+
+      Then("the application name is API Gatekeeper - Developer Sandbox")
+      val actualApplicationName = webDriver.findElement(By.className("header__menu__proposition-name")).getText
+      actualApplicationName shouldBe "API Gatekeeper - Developer Sandbox"
+
+      And("the browser window title is API Gatekeeper - Developer Sandbox - Login")
+      var actualApplicationTitle = webDriver.getTitle
+      actualApplicationTitle shouldBe "API Gatekeeper - Developer Sandbox - Login"
+
+//      And("the application header colour is rgba(40, 161, 151)")
+//      val actualHeaderColour = webDriver.findElement(By.cssSelector("#wrapper div.service-info")).getCssValue("border-top-color")
+//      actualHeaderColour.replace(" ", "") should include("rgba(40, 161, 151, 1)".replace(" ", ""))
+
+      When("the users signs in")
+      SignInPage.signIn("joe.test", "password")
+      on(ApplicationPage)
+
+      Then("the application name is API Gatekeeper - Developer Sandbox")
+      actualApplicationName shouldBe "API Gatekeeper - Developer Sandbox"
+
+      And("the browser window title is API Gatekeeper - Developer Sandbox - Applications")
+      actualApplicationTitle = webDriver.getTitle
+      actualApplicationTitle shouldBe "API Gatekeeper - Developer Sandbox - Applications"
+
+//      And("the application header colour is rgba(40, 161, 151)")
+//      actualHeaderColour.replace(" ", "") should include("rgba(40, 161, 151, 1)".replace(" ", ""))
+    }
+
+    scenario("Cookie banner is displayed on the top of the page when user first visits the website", Tag("SandboxTest")) {
+
+      Given("The developer goes to the Gatekeeper home page")
+      goOn(SignInPage)
+      on(SignInPage)
+
+      Then("the cookie banner is displayed at the very top of the page")
+      val cookieBanner = webDriver.findElement(By.id("global-cookie-message")).getLocation.toString
+      cookieBanner shouldBe "(0, 0)"
+    }
+  }
+
+  def stubApplicationList() = {
+    stubFor(get(urlEqualTo("/gatekeeper/applications"))
+      .willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
+
+    stubFor(get(urlEqualTo(s"/application")).willReturn(aResponse()
+      .withBody(applications).withStatus(200)))
+  }
+
+  def stubApplicationSubscription() = {
+    stubFor(get(urlEqualTo("/application/subscriptions"))
+      .willReturn(aResponse().withBody(applicationSubscription).withStatus(200)))
+  }
+
+  def stubApiDefinition() = {
+    stubFor(get(urlEqualTo(s"/api-definition"))
+      .willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
+  }
+
+}
