672b0960a6677d488a9e29454dff7cf048b4ea35
rawtoast <jim@jamesmorris.org>
2017-02-15 08:54:37 +0000

    Removed duplication of displayedStatus logic

--- app/model/APIDefinition.scala
+++ app/model/APIDefinition.scala
@@ -54,14 +54,7 @@ object APIDefinition {
 case class VersionSubscription(version: APIVersion, subscribed: Boolean)
 
 case class APIVersion(version: String, status: APIStatus, access: Option[APIAccess] = None) {
-  val displayedStatus = {
-    status match {
-      case APIStatus.PROTOTYPED => "Beta"
-      case APIStatus.PUBLISHED => "Stable"
-      case APIStatus.DEPRECATED => "Deprecated"
-      case APIStatus.RETIRED => "Retired"
-    }
-  }
+  val displayedStatus = APIStatus.displayedStatus(status)
 
   val accessType = access.map(_.`type`).getOrElse(APIAccessType.PUBLIC)
 }
