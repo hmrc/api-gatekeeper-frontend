6dcd8a6d4f904dd76e3827b43e18bfd650367b15
rawtoast <jim@jamesmorris.org>
2017-02-24 12:15:06 +0000

    Use application name instead of context for filtering on applications view

---
+++ test/unit/utils/SubscriptionEnhancerSpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.utils
+
+import model.{APIDefinition, APIStatus, APIVersion, ApplicationState, DetailedSubscribedApplicationResponse, SubscribedApplicationResponse}
+import org.joda.time.DateTime
+import uk.gov.hmrc.play.test.UnitSpec
+import utils.SubscriptionEnhancer
+
+
+class SubscriptionEnhancerSpec extends UnitSpec {
+
+  "SubscriptionEnhancer" when {
+
+    val subscriptionEnhancer = SubscriptionEnhancer
+
+    "Combining SubscribedApplicationResponses with ApiDefinitions" should {
+
+      val appResponses = Seq(createApplicationResponse("Hello World App", Seq("hello")),
+              createApplicationResponse("Employers App", Seq("employers-paye")),
+              createApplicationResponse("Unkown Subscription App", Seq("unknown")))
+
+      val apiDefinitions = Seq(createDefinition("Hello World", "hello"),
+        createDefinition("Employers PAYE", "employers-paye"))
+
+      val result: Seq[DetailedSubscribedApplicationResponse] = subscriptionEnhancer.combine(appResponses, apiDefinitions)
+
+      "Retain all subscriptions" in {
+        result should have size appResponses.size
+      }
+
+      "Replace any subscription contexts with SubscriptionDetails" in {
+        result.head.subscriptions.exists(_.name == "Hello World") should be(true)
+        result.tail.head.subscriptions.exists(_.name == "Employers PAYE") should be(true)
+      }
+
+      "Retain the context information" in {
+        result.head.subscriptions.exists(_.context == "hello") should be(true)
+        result.tail.head.subscriptions.exists(_.context == "employers-paye") should be(true)
+      }
+
+      "Make no changes to the subscription details if no definition can be found" in {
+        result.tail.tail.head.subscriptions.head.name should be("unknown")
+        result.tail.tail.head.subscriptions.head.context should be("unknown")
+      }
+
+      "Handle combining no application responses gracefully" in {
+        val result = subscriptionEnhancer.combine(Seq.empty, apiDefinitions)
+        result shouldBe empty
+      }
+    }
+  }
+
+  def createApplicationResponse(name:String, subs:Seq[String]): SubscribedApplicationResponse =
+    SubscribedApplicationResponse(java.util.UUID.randomUUID(), name, Some("Description"),
+      Set.empty, DateTime.now(), ApplicationState(), subs)
+
+  def createDefinition(name: String, context:String) = {
+    APIDefinition("TestService", "localhost", name, "Test Description", context,
+      Seq(APIVersion("1.0", APIStatus.PUBLISHED, None)), None)
+  }
+}
