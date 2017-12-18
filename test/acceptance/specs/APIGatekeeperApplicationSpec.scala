0cbcf67fbf55e7f3922efef3ce6240afab181a40
Pete Slater <pslater@equalexperts.com>
2017-12-18 14:36:31 +0000

    APIS-3088 - Started added tests for new application page

--- test/acceptance/specs/APIGatekeeperApplicationSpec.scala
+++ test/acceptance/specs/APIGatekeeperApplicationSpec.scala
@@ -16,12 +16,13 @@
 
 package acceptance.specs
 
-import acceptance.pages.{ApplicationPage, DashboardPage}
+import acceptance.pages.{ApplicationPage, ApplicationsPage, DashboardPage}
 import acceptance.{BaseSpec, SignInSugar}
 import com.github.tomakehurst.wiremock.client.WireMock._
 import component.matchers.CustomMatchers
 import org.openqa.selenium.By
 import org.scalatest.{GivenWhenThen, Matchers, Tag}
+
 import scala.io.Source
 
 class APIGatekeeperApplicationSpec extends BaseSpec with SignInSugar with Matchers with CustomMatchers with MockDataSugar with GivenWhenThen {
@@ -35,7 +36,7 @@ class APIGatekeeperApplicationSpec extends BaseSpec with SignInSugar with Matche
     scenario("Ensure a user can view a list of Applications", Tag("NonSandboxTest")) {
 
       Given("I have successfully logged in to the API Gatekeeper")
-      stubApplicationList()
+      stubApplicationList
       val applicationsList = Source.fromURL(getClass.getResource("/applications.json")).mkString.replaceAll("\n","")
 
       stubFor(get(urlEqualTo(s"/application")).willReturn(aResponse()
@@ -48,269 +49,66 @@ class APIGatekeeperApplicationSpec extends BaseSpec with SignInSugar with Matche
       When("I select to navigate to the Applications page")
       DashboardPage.selectApplications()
       Then("I am successfully navigated to the Applications page where I can view all developer list details by default")
-      on(ApplicationPage)
+      on(ApplicationsPage)
     }
-
-//    scenario("Ensure a user can view ALL applications") {
-//
-//      Given("I have successfully logged in to the API Gatekeeper")
-//      stubApplicationList()
-//      stubApplicationSubscription
-//      stubApiDefinition
-//      signInGatekeeper
-//      on(DashboardPage)
-//
-//      When("I select to navigate to the Applications page")
-//      DashboardPage.selectApplications()
-//      on(ApplicationPage)
-////      Thread.sleep(5000)
-//
-//      Then("all applications are successfully displayed and sorted correctly")
-//      val applications: Seq[(String, String, String,String)] = List(("A Wonderful Application", "08.04.2016","Employers paye","Pending"),
-//                                                                    ("An application for my user", "08.06.2016","Inheritance tax","Sandbox"),
-//                                                                    ("Any App", "14.04.2016","Employers paye","Approved"),
-//                                                                    ("Imrans Application", "24.03.2016", "", "Approved"),
-//                                                                    ("Mango", "21.07.2016", "Self assessment api","Pending"),
-//                                                                    ("Mark App","31.05.2016","Employers paye, Marriage allowance", "Sandbox"),
-//                                                                    ("My new app", "08.04.2016", "Individual benefits","Approved"),
-//                                                                    ("Purnimas Application", "24.03.2016", "","Approved"))
-//
-//      val allApps: Seq[((String, String, String, String), Int)] = applications.zipWithIndex
-//      //assertApplicationsList(allApps)
-//
-//      When("I select approved from the status filter drop down")
-//      ApplicationPage.selectByStatus(APPROVED)
-//
-//      Then("all the approved applications are displayed")
-//      val applications2: Seq[(String, String, String,String)]= List(("Any App", "14.04.2016","Employers paye","Approved"),
-//                                                                    ("Imrans Application", "24.03.2016", "", "Approved"),
-//                                                                    ("My new app", "08.04.2016", "Individual benefits","Approved"),
-//                                                                    ("Purnimas Application", "24.03.2016", "","Approved"))
-//
-//      val approvedApps: Seq[((String, String, String, String), Int)] = applications2.zipWithIndex
-//
-//      assertApplicationsList(approvedApps)
-//
-//      When("I select pending from the status filter drop down")
-//      ApplicationPage.selectByStatus(PENDING)
-//
-//      Then("all the pending applications are displayed")
-//      val applications3: Seq[(String, String, String,String)] = List(("A Wonderful Application", "08.04.2016","Employers paye","Pending"),
-//                                                                     ("Mango", "21.07.2016", "Self assessment api","Pending"))
-//
-//      val pendingApps: Seq[((String, String, String, String), Int)] = applications3.zipWithIndex
-//      assertApplicationsList(pendingApps)
-//
-//      When("I select sandbox from the status filter drop down")
-//      ApplicationPage.selectByStatus(SANDBOX)
-//
-//      Then("all the sandbox applications are displayed")
-//      val applications4 = List(("An application for my user","08.06.2016","","Sandbox"),
-//                               ("Mark App","31.05.2016","Employers paye, Marriage allowance", "Sandbox"))
-//
-//      val sandboxApps = applications4.zipWithIndex
-//      assertApplicationsList(sandboxApps)
-//    }
-//
-//    scenario("Ensure a user can view all applications who are subscribed to one or more API") {
-//      Given("I have successfully logged in to the API Gatekeeper")
-//      stubApplicationList()
-//      stubApplicationSubscription
-//      stubApiDefinition
-//      signInGatekeeper
-//      on(DashboardPage)
-//      DashboardPage.selectApplications()
-//      on(ApplicationPage)
-//
-//      When("I select one or more subscriptions from the filter drop down")
-//      ApplicationPage.selectBySubscription(ONEORMORESUBSCRIPTION)
-//      ApplicationPage.selectByStatus(ALL)
-//      Thread.sleep(800000)
-//
-//      Then("all applications who are subscribed to one or more APIs are successfully displayed and sorted correctly")
-//      val applications: Seq[(String, String, String,String)] = List(("A Wonderful Application", "08.04.2016","Employers paye","Pending"),
-//                                                                    ("An application for my user", "08.06.2016","Inheritance tax","Sandbox"),
-//                                                                    ("Any App", "14.04.2016","Employers paye","Approved"),
-//                                                                    ("Mango", "21.07.2016", "Self assessment api","Pending"),
-//                                                                    ("Mark App","31.05.2016","Employers paye, Marriage allowance", "Sandbox"),
-//                                                                    ("My new app", "08.04.2016", "Individual benefits","Approved"))
-//
-//      val allApps: Seq[((String, String, String, String), Int)] = applications.zipWithIndex
-//      assertApplicationsList(allApps)
-//
-//      When("I select approved from the status filter drop down")
-//      ApplicationPage.selectByStatus(APPROVED)
-//
-//      Then("all the approved applications are displayed")
-//      val applications2: Seq[(String, String, String,String)]= List(("Any App", "14.04.2016","Employers paye","Approved"),
-//                                                                    ("My new app", "08.04.2016", "Individual benefits","Approved"))
-//
-//      val approvedApps: Seq[((String, String, String, String), Int)] = applications2.zipWithIndex
-//
-//       assertApplicationsList(approvedApps)
-//
-//       When("I select pending from the status filter drop down")
-//       ApplicationPage.selectByStatus(PENDING)
-//
-//       Then("all the pending applications are displayed")
-//      val applications3: Seq[(String, String, String,String)] = List(("A Wonderful Application", "08.04.2016","Employers paye","Pending"),
-//                                                                     ("Mango", "21.07.2016", "Self assessment api","Pending"))
-//
-//       val pendingApps: Seq[((String, String, String, String), Int)] = applications3.zipWithIndex
-//       assertApplicationsList(pendingApps)
-//
-//       When("I select sandbox from the status filter drop down")
-//       ApplicationPage.selectByStatus(SANDBOX)
-//
-//       Then("all the sandbox applications are displayed")
-//      val applications4 = List(("An application for my user","08.06.2016","","Sandbox"),
-//                               ("Mark App","31.05.2016","Employers paye, Marriage allowance", "Sandbox"))
-//
-//       val sandboxApps = applications4.zipWithIndex
-//       assertApplicationsList(sandboxApps)
-//    }
-//
-//    scenario("Ensure a user can view all applications who have no subscription to an API") {
-//      Given("I have successfully logged in to the API Gatekeeper")
-//      stubApplicationListWithNoSubs
-//      stubApplicationSubscription
-//      stubApiDefinition
-//      signInGatekeeper
-//      on(DashboardPage)
-//      DashboardPage.selectApplications()
-//      on(ApplicationPage)
-//
-//      When("I select no subscription from the filter drop down")
-//      ApplicationPage.selectBySubscription(NOSUBSCRIPTION)
-//      ApplicationPage.selectByStatus(ALL)
-//
-//      Thread.sleep(90000)
-//
-//      Then("all applications who has no subscription are successfully displayed and sorted correctly")
-//      val applications: Seq[(String, String, String,String)] = List(("Imrans Application", "24.03.2016", "", "Approved"),
-//                                                                    ("Purnimas Application", "24.03.2016", "","Approved"),
-//                                                                    ("QA User App 2", "28.07.2016", "", "Pending"),
-//                                                                    ("Test Application", "28.07.2016", "", "Pending"),
-//                                                                    ("Tim", "13.04.2016", "", "Sandbox"))
-//
-//
-//      val allApps: Seq[((String, String, String, String), Int)] = applications.zipWithIndex
-//      assertApplicationsList(allApps)
-//
-//      When("I select approved from the status filter drop down")
-//      ApplicationPage.selectByStatus(APPROVED)
-//
-//      Then("all the approved applications are displayed")
-//      val applications2: Seq[(String, String, String,String)]= List(("Imrans Application", "24.03.2016", "", "Approved"),
-//                                                                    ("Purnimas Application", "24.03.2016", "","Approved"))
-//
-//      val approvedApps: Seq[((String, String, String, String), Int)] = applications2.zipWithIndex
-//
-//      assertApplicationsList(approvedApps)
-//
-//      When("I select pending from the status filter drop down")
-//      ApplicationPage.selectByStatus(PENDING)
-//
-//      Then("all the pending applications are displayed")
-//      val applications3: Seq[(String, String, String,String)] = List(("QA User App 2", "28.07.2016", "", "Pending"),
-//                                                                     ("Test Application", "28.07.2016", "", "Pending"))
-//
-//      val pendingApps: Seq[((String, String, String, String), Int)] = applications3.zipWithIndex
-//      assertApplicationsList(pendingApps)
-//
-//      When("I select sandbox from the status filter drop down")
-//      ApplicationPage.selectByStatus(SANDBOX)
-//
-//      Then("all the sandbox applications are displayed")
-//      val applications4 = List(("Tim", "13.04.2016", "", "Sandbox"))
-//
-//      val sandboxApps = applications4.zipWithIndex
-//      assertApplicationsList(sandboxApps)
-//    }
-//
-//    scenario("Ensure a user can view all applications who are subscribed to the Employers-PAYE API") {
-//      Given("I have successfully logged in to the API Gatekeeper")
-//      stubApplicationList()
-//      stubApplicationSubscription
-//      stubApiDefinition
-//      signInGatekeeper
-//      on(DashboardPage)
-//      DashboardPage.selectApplications()
-//      on(ApplicationPage)
-//
-//      Thread.sleep(90000)
-//
-//      When("I select Employers_PAYE API from the filter drop down")
-//      ApplicationPage.selectBySubscription(EMPLOYERSPAYE)
-//      ApplicationPage.selectByStatus(ALL)
-//
-//      Then("all applications subscribing to the Employers PAYE API are successfully displayed and sorted correctly")
-//      val applications: Seq[(String, String, String,String)] = List(("A Wonderful Application", "08.04.2016","Employers paye","Pending"),
-//                                                                    ("Any App", "14.04.2016","Employers paye","Approved"),
-//                                                                    ("Mark App","31.05.2016","Employers paye, Marriage allowance", "Sandbox"))
-//
-//
-//      val allApps: Seq[((String, String, String, String), Int)] = applications.zipWithIndex
-//      assertApplicationsList(allApps)
-//
-//      When("I select approved from the status filter drop down")
-//      ApplicationPage.selectByStatus(APPROVED)
-//
-//      Then("all the approved applications are displayed")
-//      val applications2: Seq[(String, String, String,String)]= List(("Any App", "14.04.2016","Employers paye","Approved"))
-//
-//      val approvedApps: Seq[((String, String, String, String), Int)] = applications2.zipWithIndex
-//
-//      assertApplicationsList(approvedApps)
-//
-//      When("I select pending from the status filter drop down")
-//      ApplicationPage.selectByStatus(PENDING)
-//
-//      Then("all the pending applications are displayed")
-//      val applications3: Seq[(String, String, String,String)] = List(("A Wonderful Application", "08.04.2016","Employers paye","Pending"))
-//
-//      val pendingApps: Seq[((String, String, String, String), Int)] = applications3.zipWithIndex
-//      assertApplicationsList(pendingApps)
-//
-//      When("I select sandbox from the status filter drop down")
-//      ApplicationPage.selectByStatus(SANDBOX)
-//
-//      Then("all the sandbox applications are displayed")
-//      val applications4 = List(("Mark App","31.05.2016","Employers paye, Marriage allowance", "Sandbox"))
-//
-//      val sandboxApps = applications4.zipWithIndex
-//      assertApplicationsList(sandboxApps)
-//    }
-
   }
 
-  def stubApplicationList() = {
-    stubFor(get(urlEqualTo("/gatekeeper/applications"))
-      .willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
+  feature("Show applications information") {
+    scenario("View a specific application") {
+      Given("I have successfully logged in to the API Gatekeeper")
+      stubApplicationList
 
-    stubFor(get(urlEqualTo(s"/application")).willReturn(aResponse()
-      .withBody(applications).withStatus(200)))
+      val applicationsList = Source.fromURL(getClass.getResource("/applications.json")).mkString.replaceAll("\n","")
+
+      stubFor(get(urlEqualTo("/application")).willReturn(aResponse().withBody(applicationsList).withStatus(200)))
+
+      stubApplicationSubscription
+      stubApiDefinition
+
+      signInGatekeeper
+      on(DashboardPage)
+
+      When("I select to navigate to the Applications page")
+      DashboardPage.selectApplications()
+
+      Then("I am successfully navigated to the Applications page where I can view all applications")
+      on(ApplicationsPage)
+
+      stubApplication
+
+      When("I select to navigate to the Automated Test Application page")
+      ApplicationsPage.selectByApplicationName("Automated Test Application")
+
+      Then("I am successfully navigated to the Automated Test Application page")
+      on(ApplicationPage)
+//      webDriver.findElement(By.id("something")).getText shouldBe "description"
+    }
   }
 
-  def stubApplicationListWithNoSubs() = {
-    stubFor(get(urlEqualTo("/gatekeeper/applications"))
-      .willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
-
-    stubFor(get(urlEqualTo(s"/application")).willReturn(aResponse()
-      .withBody(applicationWithNoSubscription).withStatus(200)))
+  def stubApplicationList = {
+    stubFor(get(urlEqualTo("/gatekeeper/applications")).willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
+    stubFor(get(urlEqualTo("/application")).willReturn(aResponse().withBody(applications).withStatus(200)))
   }
 
-  def stubApiDefinition() = {
-    stubFor(get(urlEqualTo(s"/api-definition"))
-      .willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
-
-    stubFor(get(urlEqualTo(s"/api-definition?type=private"))
-      .willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
+  def stubApplication = {
+    stubFor(get(urlEqualTo("/gatekeeper/application/fa38d130-7c8e-47d8-abc0-0374c7f73216")).willReturn(aResponse().withBody(application).withStatus(200)))
+    stubFor(get(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216")).willReturn(aResponse().withBody(application).withStatus(200)))
+    stubFor(get(urlEqualTo("/gatekeeper/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/subscription")).willReturn(aResponse().withBody("[]").withStatus(200)))
+    stubFor(get(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/subscription")).willReturn(aResponse().withBody("[]").withStatus(200)))
   }
 
-  def stubApplicationSubscription() = {
-    stubFor(get(urlEqualTo("/application/subscriptions"))
-      .willReturn(aResponse().withBody(applicationSubscription).withStatus(200)))
+  def stubApplicationListWithNoSubs = {
+    stubFor(get(urlEqualTo("/gatekeeper/applications")).willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
+    stubFor(get(urlEqualTo("/application")).willReturn(aResponse().withBody(applicationWithNoSubscription).withStatus(200)))
+  }
+
+  def stubApiDefinition = {
+    stubFor(get(urlEqualTo("/api-definition")).willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
+    stubFor(get(urlEqualTo("/api-definition?type=private")).willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
+  }
+
+  def stubApplicationSubscription = {
+    stubFor(get(urlEqualTo("/application/subscriptions")).willReturn(aResponse().withBody(applicationSubscription).withStatus(200)))
   }
 
   private def assertNumberOfApplicationsPerPage(expected: Int) = {
@@ -323,8 +121,6 @@ class APIGatekeeperApplicationSpec extends BaseSpec with SignInSugar with Matche
       val sn = webDriver.findElement(By.id(s"app-created-$index")).getText shouldBe app._2
       val em = webDriver.findElement(By.id(s"app-subs-$index")).getText shouldBe app._3
       val st = webDriver.findElement(By.id(s"app-status-$index")).getText shouldBe app._4
-   }
-
+    }
   }
-
 }
