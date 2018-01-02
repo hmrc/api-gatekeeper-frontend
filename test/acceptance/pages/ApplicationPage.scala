91b272f5e23a7a258b7a95ec7e23f0c696ba86cb
Pete Slater <pslater@equalexperts.com>
2018-01-02 09:51:36 +0000

    APIS-3089 - Fixed broken test

--- test/acceptance/pages/ApplicationPage.scala
+++ test/acceptance/pages/ApplicationPage.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import acceptance.WebPage
 
 object ApplicationPage extends WebPage {
 
-  override val url: String = "http://localhost:9000/api-gatekeeper/application/fa38d130-7c8e-47d8-abc0-0374c7f73216"
+  override val url: String = "http://localhost:9000/api-gatekeeper/applications/fa38d130-7c8e-47d8-abc0-0374c7f73216"
 
   override def isCurrentPage: Boolean = {
     currentUrl == url
