1952375290074ef35d74feef575724e0be089e55
Pete Slater <pslater@equalexperts.com>
2018-02-23 13:28:11 +0000

    APIS-3219 - Added check for non display of actions when not superuser

--- test/unit/view/applications/ApplicationViewSpec.scala
+++ test/unit/view/applications/ApplicationViewSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package unit.view.applications
 
 import java.util.UUID
@@ -12,7 +28,7 @@ import play.api.test.FakeRequest
 
 class ApplicationViewSpec extends PlaySpec with OneServerPerSuite {
   "application view" must {
-    "show application information, including Delete Application button, when logged in as superuser" in {
+    "show application information, including superuser only actions, when logged in as superuser" in {
       implicit val request = FakeRequest()
       val application =
         ApplicationResponse(
@@ -29,7 +45,29 @@ class ApplicationViewSpec extends PlaySpec with OneServerPerSuite {
       val result = views.html.applications.application.render(applicationWithHistory, Seq.empty, true, request, None, Flash.emptyCookie, applicationMessages, AppConfig)
 
       result.contentType must include("text/html")
-      result.body must contain("Delete Application")
+      result.body must include("Delete Application")
+      result.body must include("Manage</a>")
+    }
+
+    "show application information, excluding superuser only actions, when logged in as non superuser" in {
+      implicit val request = FakeRequest()
+      val application =
+        ApplicationResponse(
+          UUID.randomUUID(),
+          "application1",
+          None,
+          Set(Collaborator("sample@email.com", CollaboratorRole.ADMINISTRATOR), Collaborator("someone@email.com", CollaboratorRole.DEVELOPER)),
+          DateTime.now(),
+          Standard(),
+          ApplicationState()
+        )
+      val applicationWithHistory = ApplicationWithHistory(application, Seq.empty)
+
+      val result = views.html.applications.application.render(applicationWithHistory, Seq.empty, false, request, None, Flash.emptyCookie, applicationMessages, AppConfig)
+
+      result.contentType must include("text/html")
+      result.body mustNot include("Delete Application")
+      result.body mustNot include("Manage</a>")
     }
   }
 }
