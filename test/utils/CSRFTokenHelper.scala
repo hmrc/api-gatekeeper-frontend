fabdd8bc82c5fb5dfa4b490b07dee15f77d1b51d
Pete Slater <pslater@equalexperts.com>
2018-03-01 09:39:46 +0000

    APIS-3219 - Added form validation for app name

--- test/utils/CSRFTokenHelper.scala
+++ test/utils/CSRFTokenHelper.scala
@@ -5,7 +5,7 @@ import play.api.test.FakeRequest
 import play.filters.csrf.CSRF.Token
 
 object CSRFTokenHelper {
-  implicit class CSRFFRequestHeader(request: FakeRequest[AnyContentAsEmpty.type]) {
+  implicit class CSRFRequestHeader(request: FakeRequest[AnyContentAsEmpty.type]) {
     def withCSRFToken: FakeRequest[AnyContentAsEmpty.type] = request.copyFakeRequest(tags = request.tags ++ Map(
       Token.NameRequestTag -> "test",
       Token.RequestTag -> "test"
