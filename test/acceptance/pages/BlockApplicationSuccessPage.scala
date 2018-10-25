041e8a12ac16959adab50ab29771ea7e98090c8e
Billy Mitchell <william.mitchell@digital.hmrc.gov.uk>
2018-10-25 15:37:25 +0100

    add test for block button

--- test/acceptance/pages/BlockApplicationSuccessPage.scala
+++ test/acceptance/pages/BlockApplicationSuccessPage.scala
@@ -18,24 +18,11 @@ package acceptance.pages
 
 import acceptance.WebPage
 
-object ApplicationPage extends WebPage {
+object BlockApplicationSuccessPage extends WebPage {
 
-  override val url: String = s"http://localhost:$port/api-gatekeeper/applications/fa38d130-7c8e-47d8-abc0-0374c7f73216"
+  override val url: String = s"http://localhost:$port/api-gatekeeper/applications/fa38d130-7c8e-47d8-abc0-0374c7f73216/block"
 
   override def isCurrentPage: Boolean = {
     currentUrl == url
   }
-
-  def deleteApplicationButton = find(id("delete-application")).get
-
-  def selectDeleteApplication() = {
-    click on deleteApplicationButton
-  }
-
-  def blockApplicationButton = find(id("block-application")).get
-
-  def selectBlockApplication() = {
-    click on blockApplicationButton
-  }
-
 }
---
+++ test/acceptance/specs/APIGatekeeperBlockApplicationSpec.scala
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package acceptance.specs
+
+import acceptance.pages._
+import acceptance.{BaseSpec, SignInSugar}
+import com.github.tomakehurst.wiremock.client.WireMock._
+import component.matchers.CustomMatchers
+import org.openqa.selenium.By
+import org.scalatest.{GivenWhenThen, Matchers}
+
+import scala.io.Source
+
+class APIGatekeeperBlockApplicationSpec extends BaseSpec with SignInSugar with Matchers with CustomMatchers with MockDataSugar with GivenWhenThen {
+
+  val appName = "Automated Test Application"
+
+  feature("Block an application") {
+    scenario("I can block an application") {
+
+      stubApplicationForBlockSuccess()
+
+      When("I navigate to the Block Page for an application")
+      navigateThroughBlockApplication()
+
+      Then("I am successfully navigated to the Block Application Success page")
+      on(BlockApplicationSuccessPage)
+      assert(BlockApplicationSuccessPage.bodyText.contains("Application blocked"))
+    }
+  }
+
+  def navigateThroughBlockApplication() = {
+    Given("I have successfully logged in to the API Gatekeeper")
+    stubApplicationList()
+
+    val applicationsList = Source.fromURL(getClass.getResource("/resources/applications.json")).mkString.replaceAll("\n", "")
+
+    stubFor(get(urlEqualTo("/application")).willReturn(aResponse().withBody(applicationsList).withStatus(200)))
+
+    stubApplicationSubscription()
+    stubApiDefinition()
+
+    signInSuperUserGatekeeper
+    on(ApplicationsPage)
+
+    When("I select to navigate to the Applications page")
+    DashboardPage.selectApplications()
+
+    Then("I am successfully navigated to the Applications page where I can view all applications")
+    on(ApplicationsPage)
+
+    stubApplication()
+
+    When("I select to navigate to the Automated Test Application page")
+    ApplicationsPage.selectByApplicationName(appName)
+
+    Then("I am successfully navigated to the Automated Test Application page")
+    on(ApplicationPage)
+
+    When("I select the Block Application Button")
+    ApplicationPage.selectBlockApplication()
+
+    Then("I am successfully navigated to the Block Application page")
+    on(BlockApplicationPage)
+
+    When("I fill out the Delete Application Form correctly")
+    BlockApplicationPage.completeForm(appName)
+
+    And("I select the Delete Application Button")
+    BlockApplicationPage.selectBlockButton()
+  }
+
+  def stubApplicationList() = {
+    stubFor(get(urlEqualTo("/gatekeeper/applications")).willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
+    stubFor(get(urlEqualTo("/application")).willReturn(aResponse().withBody(applications).withStatus(200)))
+  }
+
+  def stubApplicationForDeleteSuccess() = {
+    stubFor(post(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/delete")).willReturn(aResponse().withStatus(204)))
+  }
+
+  def stubApplicationForBlockSuccess() = {
+    stubFor(post(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/block")).willReturn(aResponse().withStatus(200)))
+  }
+
+  def stubApplicationForDeleteFailure() = {
+    stubFor(post(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/delete")).willReturn(aResponse().withStatus(500)))
+  }
+
+  def stubApplication() = {
+    stubFor(get(urlEqualTo("/gatekeeper/application/fa38d130-7c8e-47d8-abc0-0374c7f73216")).willReturn(aResponse().withBody(applicationToDelete).withStatus(200)))
+    stubFor(get(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216")).willReturn(aResponse().withBody(applicationToDelete).withStatus(200)))
+    stubFor(get(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/subscription")).willReturn(aResponse().withBody("[]").withStatus(200)))
+  }
+
+  def stubApplicationListWithNoSubs() = {
+    stubFor(get(urlEqualTo("/gatekeeper/applications")).willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
+  }
+
+  def stubApiDefinition() = {
+    stubFor(get(urlEqualTo("/api-definition")).willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
+    stubFor(get(urlEqualTo("/api-definition?type=private")).willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
+  }
+
+  def stubApplicationSubscription() = {
+    stubFor(get(urlEqualTo("/application/subscriptions")).willReturn(aResponse().withBody(applicationSubscription).withStatus(200)))
+  }
+
+  private def assertNumberOfApplicationsPerPage(expected: Int) = {
+    webDriver.findElements(By.cssSelector("tbody > tr")).size() shouldBe expected
+  }
+
+  private def assertApplicationsList(devList: Seq[((String, String, String, String), Int)]) {
+    for ((app, index) <- devList) {
+      val fn = webDriver.findElement(By.id(s"app-name-$index")).getText shouldBe app._1
+      val sn = webDriver.findElement(By.id(s"app-created-$index")).getText shouldBe app._2
+      val em = webDriver.findElement(By.id(s"app-subs-$index")).getText shouldBe app._3
+      val st = webDriver.findElement(By.id(s"app-status-$index")).getText shouldBe app._4
+    }
+  }
+}
