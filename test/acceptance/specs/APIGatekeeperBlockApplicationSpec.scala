d59d93d3e013f7a48dee618bf3e0d33392df8eac
peteslater-ee <pslater@equalexperts.com>
2018-10-26 11:25:57 +0100

    API-3471 - Added test for not showinng block button

--- test/acceptance/specs/APIGatekeeperBlockApplicationSpec.scala
+++ test/acceptance/specs/APIGatekeeperBlockApplicationSpec.scala
@@ -17,33 +17,43 @@
 package acceptance.specs
 
 import acceptance.pages._
-import acceptance.{BaseSpec, SignInSugar}
 import com.github.tomakehurst.wiremock.client.WireMock._
-import component.matchers.CustomMatchers
-import org.openqa.selenium.By
-import org.scalatest.{GivenWhenThen, Matchers}
 
 import scala.io.Source
 
-class APIGatekeeperBlockApplicationSpec extends BaseSpec with SignInSugar with Matchers with CustomMatchers with MockDataSugar with GivenWhenThen {
+class APIGatekeeperBlockApplicationSpec extends APIGatekeeperBaseSpec {
 
   val appName = "Automated Test Application"
+  val blockedAppName = "Automated Test Application - Blocked"
 
   feature("Block an application") {
     scenario("I can block an application") {
-
+      stubApplication(applicationToBlock)
       stubApplicationForBlockSuccess()
 
-      When("I navigate to the Block Page for an application")
-      navigateThroughBlockApplication()
+      When("I navigate to the application page")
+      navigateToApplicationPageFor(appName)
+
+      And("I choose to block the application")
+      selectToBlockApplication()
 
       Then("I am successfully navigated to the Block Application Success page")
       on(BlockApplicationSuccessPage)
       assert(BlockApplicationSuccessPage.bodyText.contains("Application blocked"))
     }
+
+    scenario("I cannot block an application that is already blocked") {
+      stubApplication(blockedApplication)
+
+      When("I navigate to the application page")
+      navigateToApplicationPageFor(appName)
+
+      Then("I cannot see the block button")
+      ApplicationPage.bodyText.contains("Block application") shouldBe false
+    }
   }
 
-  def navigateThroughBlockApplication() = {
+  def navigateToApplicationPageFor(applicationName: String) = {
     Given("I have successfully logged in to the API Gatekeeper")
     stubApplicationList()
 
@@ -53,88 +63,6 @@ class APIGatekeeperBlockApplicationSpec extends BaseSpec with SignInSugar with M
 
     stubApplicationSubscription()
     stubApiDefinition()
-    s"""
-       |{
-       |  "application": {
-       |    "id": "$appToDelete",
-       |    "clientId": "clientid1",
-       |    "name": "Automated Test Application",
-       |    "description": "$applicationDescription",
-       |    "deployedTo": "PRODUCTION",
-       |    "collaborators": [
-       |      {
-       |        "emailAddress": "$adminEmail",
-       |        "role": "ADMINISTRATOR"
-       |      },
-       |      {
-       |        "emailAddress": "$developer",
-       |        "role": "DEVELOPER"
-       |      },
-       |      {
-       |        "emailAddress": "$developer8",
-       |        "role": "DEVELOPER"
-       |      }
-       |    ],
-       |    "createdOn": 1459866628433,
-       |    "redirectUris": [],
-       |    "termsAndConditionsUrl": "http://www.example.com/termsAndConditions",
-       |    "privacyPolicyUrl": "http://www.example.com/privacy",
-       |    "access": {
-       |      "redirectUris": [],
-       |      "overrides": [],
-       |      "termsAndConditionsUrl": "http://localhost:22222/terms",
-       |      "privacyPolicyUrl": "http://localhost:22222/privacy",
-       |      "accessType": "STANDARD"
-       |    },
-       |    "state": {
-       |      "name": "PRODUCTION",
-       |      "requestedByEmailAddress": "$adminEmail",
-       |      "verificationCode": "pRoPW05BMTQ_HqzTTR0Ent10py9gvstX34_a3dxx4V8",
-       |      "updatedOn": 1459868573962
-       |    },
-       |    "rateLimitTier": "BRONZE",
-       |    "checkInformation": {
-       |      "contactDetails": {
-       |        "fullname": "Holly Golightly",
-       |        "email": "holly.golightly@example.com",
-       |        "telephoneNumber": "020 1122 3344"
-       |      },
-       |      "confirmedName": true,
-       |      "providedPrivacyPolicyURL": true,
-       |      "providedTermsAndConditionsURL": true,
-       |      "applicationDetails": "An application that is pending approval",
-       |      "termsOfUseAgreements": [{
-       |        "emailAddress": "test@example.com",
-       |        "timeStamp": 1459868573962,
-       |        "version": "1.0"
-       |      }]
-       |
-       |    },
-       |    "subscriptions": [],
-       |    "blocked": false
-       |  },
-       |  "history": [
-       |      {
-       |      "applicationId": "a6d37b4a-0a80-4b7f-b150-5f8f99fe27ea",
-       |      "state": "PENDING_GATEKEEPER_APPROVAL",
-       |      "actor": {
-       |        "id": "$adminEmail",
-       |        "actorType": "COLLABORATOR"
-       |      },
-       |      "changedAt": 1458659208000
-       |    },
-       |    {
-       |      "applicationId": "a6d37b4a-0a80-4b7f-b150-5f8f99fe27ea",
-       |      "state": "PENDING_REQUESTER_VERIFICATION",
-       |      "actor": {
-       |        "id": "gatekeeper.username",
-       |        "actorType": "GATEKEEPER"
-       |      },
-       |      "changedAt": 1459868522961
-       |    }
-       |  ]
-       |}
-    """.stripMargin
 
     signInSuperUserGatekeeper
     on(ApplicationsPage)
@@ -145,16 +73,14 @@ class APIGatekeeperBlockApplicationSpec extends BaseSpec with SignInSugar with M
     Then("I am successfully navigated to the Applications page where I can view all applications")
     on(ApplicationsPage)
 
-    stubApplication()
-
     When("I select to navigate to the Automated Test Application page")
-    ApplicationsPage.selectByApplicationName(appName)
+    ApplicationsPage.selectByApplicationName(applicationName)
 
     Then("I am successfully navigated to the Automated Test Application page")
     on(ApplicationPage)
+  }
 
-    Thread.sleep(2000)
-
+  def selectToBlockApplication() = {
     When("I select the Block Application Button")
     ApplicationPage.selectBlockApplication()
 
@@ -168,44 +94,7 @@ class APIGatekeeperBlockApplicationSpec extends BaseSpec with SignInSugar with M
     BlockApplicationPage.selectBlockButton()
   }
 
-  def stubApplicationList() = {
-    stubFor(get(urlEqualTo("/gatekeeper/applications")).willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
-    stubFor(get(urlEqualTo("/application")).willReturn(aResponse().withBody(applications).withStatus(200)))
-  }
-
   def stubApplicationForBlockSuccess() = {
     stubFor(post(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/block")).willReturn(aResponse().withStatus(200)))
-  }
-
-  def stubApplication() = {
-    stubFor(get(urlEqualTo("/gatekeeper/application/fa38d130-7c8e-47d8-abc0-0374c7f73216")).willReturn(aResponse().withBody(applicationToBlock).withStatus(200)))
-    stubFor(get(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216")).willReturn(aResponse().withBody(applicationToBlock).withStatus(200)))
-    stubFor(get(urlEqualTo("/application/fa38d130-7c8e-47d8-abc0-0374c7f73216/subscription")).willReturn(aResponse().withBody("[]").withStatus(200)))
-  }
-
-  def stubApplicationListWithNoSubs() = {
-    stubFor(get(urlEqualTo("/gatekeeper/applications")).willReturn(aResponse().withBody(approvedApplications).withStatus(200)))
-  }
-
-  def stubApiDefinition() = {
-    stubFor(get(urlEqualTo("/api-definition")).willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
-    stubFor(get(urlEqualTo("/api-definition?type=private")).willReturn(aResponse().withStatus(200).withBody(apiDefinition)))
-  }
-
-  def stubApplicationSubscription() = {
-    stubFor(get(urlEqualTo("/application/subscriptions")).willReturn(aResponse().withBody(applicationSubscription).withStatus(200)))
-  }
-
-  private def assertNumberOfApplicationsPerPage(expected: Int) = {
-    webDriver.findElements(By.cssSelector("tbody > tr")).size() shouldBe expected
-  }
-
-  private def assertApplicationsList(devList: Seq[((String, String, String, String), Int)]) {
-    for ((app, index) <- devList) {
-      val fn = webDriver.findElement(By.id(s"app-name-$index")).getText shouldBe app._1
-      val sn = webDriver.findElement(By.id(s"app-created-$index")).getText shouldBe app._2
-      val em = webDriver.findElement(By.id(s"app-subs-$index")).getText shouldBe app._3
-      val st = webDriver.findElement(By.id(s"app-status-$index")).getText shouldBe app._4
-    }
   }
 }
