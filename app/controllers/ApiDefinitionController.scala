891b47bf27276aaa569222a9d8e0bf13cc825a12
Pete Slater <pslater@equalexperts.com>
2020-06-16 15:25:24 +0100

    APIS-4786 - Got api defs endpoint working.
    Co-authored-by: Adam Pridmore <adam_pridmore@hotmail.com>

--- app/controllers/ApiDefinitionController.scala
+++ app/controllers/ApiDefinitionController.scala
@@ -20,15 +20,33 @@ import config.AppConfig
 import javax.inject.Inject
 import play.api.mvc.Action
 import services.ApiDefinitionService
+import model._
 
 import scala.concurrent.ExecutionContext
-import scala.concurrent.Future.successful
+
+case class ApiDefinitionView(apiName: String, apiVersion: String, status: String)
 
 class ApiDefinitionController @Inject()(apiDefinitionService: ApiDefinitionService)
                                        (implicit override val appConfig: AppConfig, val ec: ExecutionContext)
   extends BaseController {
 
   def something() = Action.async { implicit request =>
-    successful(Ok(""))
+    val definitions = apiDefinitionService.fetchAllApiDefinitions()
+
+    // TODO: Sorting
+    // Environment
+
+    definitions.map(allDefinitions => {
+      val allDefinitionsAsRows = allDefinitions
+        .flatMap(d => toViewModel(d))
+        .map(vm => s"${vm.apiName},${vm.apiVersion},${vm.status}")
+      Ok(allDefinitionsAsRows.mkString(System.lineSeparator()))
+    })
+  }
+
+  private def toViewModel(apiDefinition: APIDefinition): Seq[ApiDefinitionView] = {
+    apiDefinition.versions.map(v => 
+      ApiDefinitionView(apiDefinition.name, v.version, v.displayedStatus)
+    )
   }
 }
