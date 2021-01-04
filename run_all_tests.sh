af17fe28468be4e910ada36d083ef432e83e9204
mattgreenuk <2297445+mattgreenuk@users.noreply.github.com>
2021-01-04 14:52:47 +0000

    [APIS-5134] removed mem switch to make sbt run ok

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sbt -mem 4000 clean compile coverage test it:test acceptance:test sandbox:test coverageReport
+sbt clean compile coverage test it:test acceptance:test sandbox:test coverageReport
 python dependencyReport.py api-gatekeeper-frontend
