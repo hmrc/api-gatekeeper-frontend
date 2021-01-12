4364c83ab534cb4deae4b9521e3930b04e30a14c
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2021-01-12 13:40:25 +0000

    APIS-4925-Verbs - More changes

--- test/connectors/SubscriptionFieldsConnectorSpec.scala
+++ test/connectors/SubscriptionFieldsConnectorSpec.scala
@@ -20,20 +20,18 @@ import java.util.UUID
 
 import config.AppConfig
 import connectors.SubscriptionFieldsConnector._
-import model.Environment.Environment
+import com.github.tomakehurst.wiremock.client.WireMock._
 import model.SubscriptionFields._
-import model.{ApiIdentifier, ApiContext, ApiVersion, ClientId, Environment, FieldsDeleteFailureResult, FieldsDeleteSuccessResult}
-import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
+import model._
 import org.scalatest.concurrent.ScalaFutures
-import play.api.http.Status.{ACCEPTED, INTERNAL_SERVER_ERROR, NO_CONTENT, OK}
-import uk.gov.hmrc.http.{HttpResponse, _}
+import play.api.http.Status._
+import uk.gov.hmrc.http._
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
 import uk.gov.hmrc.play.test.UnitSpec
-
-import scala.concurrent.{ExecutionContext, Future}
-import model.FieldName
-import model.FieldValue
+import JsonFormatters._
 import uk.gov.hmrc.play.test.WithFakeApplication
+import play.api.libs.json.Json
+import org.mockito.MockitoSugar
 
 class SubscriptionFieldsConnectorSpec 
     extends UnitSpec 
@@ -59,24 +57,17 @@ class SubscriptionFieldsConnectorSpec
   private val apiIdentifier = ApiIdentifier(apiContext, apiVersion)
   private val fieldsId = UUID.randomUUID()
   private val urlPrefix = "/field"
-  private val upstream500Response = Upstream5xxResponse("", INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR)
 
   def subscriptionFieldsBaseUrl(clientId: ClientId) = s"$urlPrefix/application/${clientId.value}"
 
   trait Setup {
     implicit val ec = scala.concurrent.ExecutionContext.Implicits.global
 
-    val apiKey: String = UUID.randomUUID().toString
-    val bearerToken: String = UUID.randomUUID().toString
-    val http = fakeApplication.injector.instanceOf[HttpClient]
-    val mockHttpClient: HttpClient = mock[HttpClient]
-    val mockProxiedHttpClient: ProxiedHttpClient = mock[ProxiedHttpClient]
+    val httpClient = fakeApplication.injector.instanceOf[HttpClient]
     val mockAppConfig: AppConfig = mock[AppConfig]
+    when(mockAppConfig.subscriptionFieldsProductionBaseUrl).thenReturn(wireMockUrl)
 
-    when(mockAppConfig.subscriptionFieldsSandboxApiKey).thenReturn(apiKey)
-
-    val subscriptionFieldsConnector = new ProductionSubscriptionFieldsConnector(mockAppConfig, mockHttpClient)
-
+    val subscriptionFieldsConnector = new ProductionSubscriptionFieldsConnector(mockAppConfig, httpClient)
   }
 
   "fetchFieldsValuesWithPrefetchedDefinitions" should {
@@ -87,12 +78,19 @@ class SubscriptionFieldsConnectorSpec
 
     val prefetchedDefinitions = Map(apiIdentifier -> Seq(subscriptionDefinition))
 
-    val getUrl = s"${subscriptionFieldsBaseUrl(clientId)}/context/${apiContext.urlEncode()}/version/${apiVersion.urlEncode()}"
+    val getUrl = s"${subscriptionFieldsBaseUrl(clientId)}/context/${apiContext.value}/version/${apiVersion.value}"
 
     "return subscription fields for an API" in new Setup {
-      when(mockHttpClient
-        .GET[ApplicationApiFieldValues](eqTo(getUrl))(*, *, *))
-        .thenReturn(Future.successful(subscriptionFields))
+      val payload = Json.toJson(subscriptionFields).toString
+
+      stubFor(
+        get(urlEqualTo(getUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(OK)
+          .withBody(payload)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.fetchFieldsValuesWithPrefetchedDefinitions(clientId, apiIdentifier, prefetchedDefinitions))
 
@@ -101,18 +99,27 @@ class SubscriptionFieldsConnectorSpec
 
     "fail when api-subscription-fields returns a 500" in new Setup {
 
-      when(mockHttpClient.GET[ApplicationApiFieldValues](eqTo(getUrl))(*, *, *))
-        .thenReturn(Future.failed(upstream500Response))
+      stubFor(
+        get(urlEqualTo(getUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(INTERNAL_SERVER_ERROR)
+        )
+      )
 
       intercept[UpstreamErrorResponse] {
         await(subscriptionFieldsConnector.fetchFieldsValuesWithPrefetchedDefinitions(clientId, apiIdentifier, prefetchedDefinitions))
-      }
+      }.statusCode shouldBe INTERNAL_SERVER_ERROR
     }
 
     "return empty when api-subscription-fields returns a 404" in new Setup {
-
-      when(mockHttpClient.GET[ApplicationApiFieldValues](eqTo(getUrl))(*, *, *))
-        .thenReturn(Future.failed(new NotFoundException("")))
+      stubFor(
+        get(urlEqualTo(getUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(NOT_FOUND)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.fetchFieldsValuesWithPrefetchedDefinitions(clientId, apiIdentifier, prefetchedDefinitions))
       result shouldBe Seq(subscriptionFieldValue.copy(value = FieldValue.empty))
@@ -125,11 +132,16 @@ class SubscriptionFieldsConnectorSpec
 
     "return all field definitions" in new Setup {
 
-      private val validResponse = AllApiFieldDefinitions(apis = Seq(ApiFieldDefinitions(apiContext, apiVersion, definitions)))
+      private val validResponse = Json.toJson(AllApiFieldDefinitions(apis = Seq(ApiFieldDefinitions(apiContext, apiVersion, definitions)))).toString
 
-      when(mockHttpClient.GET[AllApiFieldDefinitions](eqTo(url))(*, *, *))
-        .thenReturn(Future.successful(validResponse))
-
+      stubFor(
+        get(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(OK)
+          .withBody(validResponse)
+        )
+      )
       private val result = await (subscriptionFieldsConnector.fetchAllFieldDefinitions())
 
       val expectedResult = Map(apiIdentifier -> definitions.map(toDomain))
@@ -138,36 +150,49 @@ class SubscriptionFieldsConnectorSpec
     }
 
     "fail when api-subscription-fields returns a 500" in new Setup {
-
-      when(mockHttpClient.GET[AllApiFieldDefinitions](eqTo(url))(*, *, *))
-        .thenReturn(Future.failed(upstream500Response))
+      stubFor(
+        get(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(INTERNAL_SERVER_ERROR)
+        )
+      )
 
       intercept[UpstreamErrorResponse] {
         await(subscriptionFieldsConnector.fetchAllFieldDefinitions())
-      }
+      }.statusCode shouldBe INTERNAL_SERVER_ERROR
     }
 
     "fail when api-subscription-fields returns unexpected response" in new Setup {
-
-      when(mockHttpClient.GET[AllApiFieldDefinitions](eqTo(url))(*, *, *))
-        .thenReturn(Future.failed(new NotFoundException("")))
+      stubFor(
+        get(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(NOT_FOUND)
+        )
+      )
 
       private val result = await (subscriptionFieldsConnector.fetchAllFieldDefinitions())
-
       result shouldBe Map.empty[String,String]
     }
   }
 
   "fetchFieldDefinitions" should {
-    val url = s"/definition/context/${apiContext.urlEncode()}/version/${apiVersion.urlEncode()}"
+    val url = s"/definition/context/${apiContext.value}/version/${apiVersion.value}"
 
     val expectedDefinitions = List(expectedSubscriptionDefinition)
 
-    val validResponse = ApiFieldDefinitions(apiContext, apiVersion, definitionsFromRestService)
+    val validResponse = Json.toJson(ApiFieldDefinitions(apiContext, apiVersion, definitionsFromRestService)).toString
 
     "return definitions" in new Setup {
-      when(mockHttpClient.GET[ApiFieldDefinitions](eqTo(url))(*, *, *))
-        .thenReturn(Future.successful(validResponse))
+      stubFor(
+        get(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(OK)
+          .withBody(validResponse)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.fetchFieldDefinitions(apiContext, apiVersion))
 
@@ -175,9 +200,13 @@ class SubscriptionFieldsConnectorSpec
     }
 
     "fail when api-subscription-fields returns a 500" in new Setup {
-
-      when(mockHttpClient.GET[ApiFieldDefinitions](eqTo(url))(*, *, *))
-        .thenReturn(Future.failed(upstream500Response))
+      stubFor(
+        get(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(INTERNAL_SERVER_ERROR)
+        )
+      )
 
       intercept[UpstreamErrorResponse] {
         await(subscriptionFieldsConnector.fetchFieldDefinitions(apiContext, apiVersion))
@@ -189,9 +218,7 @@ class SubscriptionFieldsConnectorSpec
     val definitionsUrl = s"/definition/context/${apiContext.urlEncode()}/version/${apiVersion.urlEncode()}"
     val valuesUrl = s"/field/application/${clientId.value}/context/${apiContext.urlEncode()}/version/${apiVersion.urlEncode()}"
 
-    
-
-    val validDefinitionsResponse: ApiFieldDefinitions = ApiFieldDefinitions(apiContext, apiVersion, definitionsFromRestService)
+    val validDefinitionsResponse = Json.toJson(ApiFieldDefinitions(apiContext, apiVersion, definitionsFromRestService)).toString
 
     "return field values" in new Setup {
       val expectedDefinitions = definitionsFromRestService.map(d => SubscriptionFieldDefinition(d.name, d.description, d.hint, d.`type`, d.shortDescription))
@@ -199,13 +226,25 @@ class SubscriptionFieldsConnectorSpec
 
       val fieldsValues: Map[FieldName, FieldValue] = fields(expectedFieldValues.map(v => v.definition.name -> v.value): _*)
 
-      val validValuesResponse: ApplicationApiFieldValues = ApplicationApiFieldValues(clientId, apiContext, apiVersion, fieldsId, fieldsValues)
+      val validValuesResponse = Json.toJson(ApplicationApiFieldValues(clientId, apiContext, apiVersion, fieldsId, fieldsValues)).toString
 
-      when(mockHttpClient.GET[ApiFieldDefinitions](eqTo(definitionsUrl))(*, *, *))
-        .thenReturn(Future.successful(validDefinitionsResponse))
+      stubFor(
+        get(urlEqualTo(definitionsUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(OK)
+          .withBody(validDefinitionsResponse)
+        )
+      )
 
-      when(mockHttpClient.GET[ApplicationApiFieldValues](eqTo(valuesUrl))(*, *, *))
-        .thenReturn(Future.successful(validValuesResponse))
+      stubFor(
+        get(urlEqualTo(valuesUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(OK)
+          .withBody(validValuesResponse)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.fetchFieldValues(clientId, apiContext, apiVersion))
 
@@ -213,21 +252,35 @@ class SubscriptionFieldsConnectorSpec
     }
 
     "fail when fetching field definitions returns a 500" in new Setup {
-      when(mockHttpClient.GET[ApiFieldDefinitions](eqTo(definitionsUrl))(*, *, *))
-        .thenReturn(Future.failed(upstream500Response))
+      stubFor(
+        get(urlEqualTo(definitionsUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(INTERNAL_SERVER_ERROR)
+        )
+      )
 
       intercept[UpstreamErrorResponse] {
         await(subscriptionFieldsConnector.fetchFieldValues(clientId, apiContext, apiVersion))
-      }
+      }.statusCode shouldBe INTERNAL_SERVER_ERROR
     }
 
     "fail when fetching field definition values returns a 500" in new Setup {
-      when(mockHttpClient.GET[ApiFieldDefinitions](eqTo(definitionsUrl))(*, *, *))
-        .thenReturn(Future.successful(validDefinitionsResponse))
-
-      when(mockHttpClient.GET[ApiFieldDefinitions](eqTo(valuesUrl))(*, *, *))
-        .thenReturn(Future.failed(upstream500Response))
-
+      stubFor(
+        get(urlEqualTo(definitionsUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(OK)
+          .withBody(validDefinitionsResponse)
+        )
+      )
+      stubFor(
+        get(urlEqualTo(valuesUrl))
+        .willReturn(
+          aResponse()
+          .withStatus(INTERNAL_SERVER_ERROR)
+        )
+      )
       intercept[UpstreamErrorResponse] {
         await(subscriptionFieldsConnector.fetchFieldValues(clientId, apiContext, apiVersion))
       }
@@ -235,39 +288,51 @@ class SubscriptionFieldsConnectorSpec
   }
 
   "saveFieldValues" should {
-
     val fieldsValues = fields(FieldName.random -> FieldValue.random, FieldName.random -> FieldValue.random)
-    val subFieldsPutRequest = SubscriptionFieldsPutRequest(clientId, apiContext, apiVersion, fieldsValues)
+    val subFieldsPutRequest = Json.toJson(SubscriptionFieldsPutRequest(clientId, apiContext, apiVersion, fieldsValues)).toString
 
     val putUrl = s"${subscriptionFieldsBaseUrl(clientId)}/context/${apiContext.urlEncode()}/version/${apiVersion.urlEncode()}"
 
     "save the fields" in new Setup {
-      when(mockHttpClient.PUT[SubscriptionFieldsPutRequest, HttpResponse](eqTo(putUrl), eqTo(subFieldsPutRequest), *)(*, *, *, *))
-        .thenReturn(Future.successful(HttpResponse(OK)))
+      stubFor(
+        put(urlEqualTo(putUrl))
+        .withRequestBody(equalTo(subFieldsPutRequest))
+        .willReturn(
+          aResponse()
+          .withStatus(OK)
+        )
+      )
+      private val result = await(subscriptionFieldsConnector.saveFieldValues(clientId, apiContext, apiVersion, fieldsValues))
 
-      await(subscriptionFieldsConnector.saveFieldValues(clientId, apiContext, apiVersion, fieldsValues))
-
-      verify(mockHttpClient).PUT[SubscriptionFieldsPutRequest, HttpResponse](eqTo(putUrl), eqTo(subFieldsPutRequest), *)(*, *, *, *)
+      result shouldBe SaveSubscriptionFieldsSuccessResponse
     }
 
     "fail when api-subscription-fields returns a 500" in new Setup {
-
-      when(mockHttpClient.PUT[SubscriptionFieldsPutRequest, HttpResponse](*, *, *)(*, *, *, *))
-        .thenReturn(Future.failed(upstream500Response))
-
+      stubFor(
+        put(urlEqualTo(putUrl))
+        .withRequestBody(equalTo(subFieldsPutRequest))
+        .willReturn(
+          aResponse()
+          .withStatus(INTERNAL_SERVER_ERROR)
+        )
+      )
       intercept[UpstreamErrorResponse] {
         await(subscriptionFieldsConnector.saveFieldValues(clientId, apiContext, apiVersion, fieldsValues))
       }
     }
 
     "fail when api-subscription-fields returns a 404" in new Setup {
-
-      when(mockHttpClient.PUT[SubscriptionFieldsPutRequest, HttpResponse](*, *, *)(*,*,*,*))
-        .thenReturn(Future.failed(new NotFoundException("")))
-
-      intercept[NotFoundException] {
+      stubFor(
+        put(urlEqualTo(putUrl))
+        .withRequestBody(equalTo(subFieldsPutRequest))
+        .willReturn(
+          aResponse()
+          .withStatus(NOT_FOUND)
+        )
+      )
+      intercept[UpstreamErrorResponse] {
         await(subscriptionFieldsConnector.saveFieldValues(clientId, apiContext, apiVersion, fieldsValues))
-      }
+      }.statusCode shouldBe NOT_FOUND
     }
   }
 
@@ -276,9 +341,13 @@ class SubscriptionFieldsConnectorSpec
     val url = s"${subscriptionFieldsBaseUrl(clientId)}/context/${apiContext.urlEncode()}/version/${apiVersion.urlEncode()}"
 
     "return success after delete call has returned 204 NO CONTENT" in new Setup {
-
-      when(mockHttpClient.DELETE(url))
-        .thenReturn(Future.successful(HttpResponse(NO_CONTENT)))
+      stubFor(
+        delete(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(NO_CONTENT)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.deleteFieldValues(clientId, apiContext, apiVersion))
 
@@ -286,9 +355,13 @@ class SubscriptionFieldsConnectorSpec
     }
 
     "return failure if api-subscription-fields returns unexpected status" in new Setup {
-
-      when(mockHttpClient.DELETE(url))
-        .thenReturn(Future.successful(HttpResponse(ACCEPTED)))
+      stubFor(
+        delete(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(ACCEPTED)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.deleteFieldValues(clientId, apiContext, apiVersion))
 
@@ -296,9 +369,13 @@ class SubscriptionFieldsConnectorSpec
     }
 
     "return failure when api-subscription-fields returns a 500" in new Setup {
-
-      when(mockHttpClient.DELETE(url))
-        .thenReturn(Future.failed(upstream500Response))
+      stubFor(
+        delete(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(INTERNAL_SERVER_ERROR)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.deleteFieldValues(clientId, apiContext, apiVersion))
 
@@ -306,9 +383,13 @@ class SubscriptionFieldsConnectorSpec
     }
 
     "return success when api-subscription-fields returns a 404" in new Setup {
-
-      when(mockHttpClient.DELETE(url))
-        .thenReturn(Future.failed(new NotFoundException("")))
+      stubFor(
+        delete(urlEqualTo(url))
+        .willReturn(
+          aResponse()
+          .withStatus(NOT_FOUND)
+        )
+      )
 
       private val result = await(subscriptionFieldsConnector.deleteFieldValues(clientId, apiContext, apiVersion))
       result shouldBe FieldsDeleteSuccessResult
