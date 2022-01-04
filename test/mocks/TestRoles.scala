edc5c77e4f04d4736fdcc9f282aae042588a1798
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2022-01-04 10:11:56 +0000

    Changes to test files

--- test/mocks/TestRoles.scala
+++ test/mocks/TestRoles.scala
@@ -17,7 +17,7 @@
 package mocks
 
 object TestRoles {
-  val userRole = s"user-role"
-  val adminRole = s"admin-role"
-  val superUserRole = s"super-user-role"
+  val userRole = "user-role"
+  val adminRole = "admin-role"
+  val superUserRole = "super-user-role"
 }
