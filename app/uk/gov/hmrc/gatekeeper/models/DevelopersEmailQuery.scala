814ea8c26b871a5a4ad8f7db57b336b7c660a76e
srinivasalumunagala <srinivasalu.munagala@digital.hmrc.gov.uk>
2022-07-28 23:11:36 +0100

    APIDF-506 some UI code 4

--- app/uk/gov/hmrc/gatekeeper/models/DevelopersEmailQuery.scala
+++ app/uk/gov/hmrc/gatekeeper/models/DevelopersEmailQuery.scala
@@ -19,14 +19,16 @@ package uk.gov.hmrc.gatekeeper.models
 import play.api.libs.json.Json
 
 
+case class EmailOverride(email: List[RegisteredUser], isOverride: Boolean = false)
 case class DevelopersEmailQuery(topic: Option[String] = None,
                                 apis: Option[Seq[String]] = None,
                                 apiCategories: Option[Seq[APICategory]] = None,
                                 privateapimatch: Boolean = false,
                                 apiVersionFilter: Option[String] = None,
                                 allUsers: Boolean = false,
-                                emailsForSomeCases: Option[List[RegisteredUser]] = None)
+                                emailsForSomeCases: Option[EmailOverride] = None)
 
 object DevelopersEmailQuery {
+  implicit val emailOverrideFormatter = Json.format[EmailOverride]
   implicit val formatDevelopersEmailQuery = Json.format[DevelopersEmailQuery]
 }
