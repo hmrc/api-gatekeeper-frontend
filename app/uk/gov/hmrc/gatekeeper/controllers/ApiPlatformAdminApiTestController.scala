a2cec4943695963471261d3ffb5d08aaa78e38f8
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2023-12-20 10:48:02 +0000

    APIS-6537 Add test endpoint to call api-platform-admin-api (#599)
    * APIS-6537 Add test endpoint to call api-platform-admin-api
    
    * APIS-6537 Add test endpoint to call api-platform-admin-api
    
    * APIS-6537 PR bot action

---
+++ app/uk/gov/hmrc/gatekeeper/controllers/ApiPlatformAdminApiTestController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gatekeeper.controllers
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.ApplicationId
+import uk.gov.hmrc.apiplatform.modules.gkauth.controllers.GatekeeperBaseController
+import uk.gov.hmrc.apiplatform.modules.gkauth.controllers.actions.GatekeeperAuthorisationActions
+import uk.gov.hmrc.apiplatform.modules.gkauth.services.{LdapAuthorisationService, StrideAuthorisationService}
+import uk.gov.hmrc.gatekeeper.connectors.ApiPlatformAdminApiConnector
+
+@Singleton
+class ApiPlatformAdminApiTestController @Inject() (
+    mcc: MessagesControllerComponents,
+    connector: ApiPlatformAdminApiConnector,
+    strideAuthorisationService: StrideAuthorisationService,
+    val ldapAuthorisationService: LdapAuthorisationService
+  )(implicit ec: ExecutionContext
+  ) extends GatekeeperBaseController(strideAuthorisationService, mcc) with GatekeeperAuthorisationActions {
+
+  def page(applicationId: ApplicationId): Action[AnyContent] = anyAuthenticatedUserAction { implicit request =>
+    for {
+      application <- connector.getApplication(applicationId, hc)
+    } yield Ok(application.fold("Application not found")(app => app.asJson.toString()))
+  }
+}
