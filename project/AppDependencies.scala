a453d191daf125f149734e0529e6ebb5f185b67d
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-07-11 20:43:17 +0100

    APIS-7017 Bump application domain library version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
 
   val apiDomainVersion    = "0.15.0"
   val commonDomainVersion = "0.13.0"
-  val appDomainVersion    = "0.53.0"
+  val appDomainVersion    = "0.54.0"
 
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
 
