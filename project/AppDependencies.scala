f86fde007ad06307dd93e197c7ce7a837fb9538b
Neil Frow <675806+worthydolt@users.noreply.github.com>
2024-08-22 17:55:19 +0100

    API-7700: updating test data to include lastActionActor

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   lazy val bootstrapVersion  = "9.2.0"
 
   val apiDomainVersion    = "0.15.0"
-  val appDomainVersion    = "0.54.0"
+  val appDomainVersion    = "0.57.0"
   val tpdDomainVersion    = "0.5.0"
 
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
