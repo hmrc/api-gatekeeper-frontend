b4a4403518ab87b9b6e55754052c446d1538c9c7
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-03-11 08:24:27 +0000

    API-8255 - PR Bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ object AppDependencies {
 
   lazy val jsoupVersion      = "1.12.1"
   lazy val scalaCheckVersion = "1.17.0"
-  lazy val bootstrapVersion  = "9.10.0"
+  lazy val bootstrapVersion  = "9.11.0"
 
-  val tpdDomainVersion    = "0.11.0"
+  val tpdDomainVersion    = "0.12.0"
   val apiDomainVersion    = "0.19.1"
   val appDomainVersion    = "0.75.0"
 
@@ -14,13 +14,13 @@ object AppDependencies {
 
   lazy val dependencies = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.12.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.1.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.0.0",
+    "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.2.0",
     "uk.gov.hmrc"       %% "emailaddress"                          % "3.8.0",
     "commons-net"        % "commons-net"                           % "3.9.0",
     "org.apache.commons" % "commons-csv"                           % "1.10.0",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "3.0.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "3.1.0",
     "uk.gov.hmrc"       %% "api-platform-application-domain"       % appDomainVersion,
     "uk.gov.hmrc"       %% "api-platform-api-domain"               % apiDomainVersion,
     "uk.gov.hmrc"       %% "api-platform-tpd-domain"               % tpdDomainVersion
@@ -29,7 +29,7 @@ object AppDependencies {
   lazy val testDependencies: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"                    % bootstrapVersion,
     "org.jsoup"               % "jsoup"                                     % jsoupVersion,
-    "uk.gov.hmrc"            %% "ui-test-runner"                            % "0.33.0",
+    "uk.gov.hmrc"            %% "ui-test-runner"                            % "0.45.0",
     "org.mockito"            %% "mockito-scala-scalatest"                   % "1.17.30",
     "org.scalacheck"         %% "scalacheck"                                % scalaCheckVersion,
     "uk.gov.hmrc"            %% "api-platform-test-tpd-domain"              % tpdDomainVersion,
