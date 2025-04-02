18fb51e72a833bb0949d5608e6ec3d0273c2d7d2
anjumabbas <khaakpaayali@gmail.com>
2025-04-02 13:26:31 +0100

    APIS-7479 - PR Bot upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,7 @@ object AppDependencies {
 
   lazy val dependencies = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.12.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.13.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.2.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.2.0",
     "commons-net"        % "commons-net"                           % "3.9.0",
