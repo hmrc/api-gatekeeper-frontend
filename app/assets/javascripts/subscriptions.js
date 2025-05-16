b41d2e9b6e2caa4670f7eb2c2f164c69237d3c4c
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-05-16 09:51:33 +0100

    API-8389 - Update the gatekeeper subscriptions page to use XHR (#714)

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,22 +1,100 @@
 (function() {
-  function subscribe(event) {
-    const offButton = event.target.id.substr(0, event.target.id.length-2) + 'off';
-    document.getElementById(event.target.id).checked = true;
-    document.getElementById(offButton).checked = false;
-    document.getElementById(offButton).removeAttribute('checked');
+  const incrementFunction = function(num) { return num + 1; }
+  const decrementFunction = function(num) { return num - 1; }
 
-    const subscribeForm = event.target.parentElement;
-    subscribeForm.submit();
+  function getParents(elem) {
+    var parents = [];
+    while(elem.parentNode && elem.parentNode.nodeName.toLowerCase() !== 'body') {
+      elem = elem.parentNode;
+      parents.push(elem);
+    }
+
+    return parents;
+  }
+
+  function updateSubscriptionCount(form, countUpdateFunction) {
+    const topLevelElement = getParents(form).find((node) => node.id.endsWith("-accordion-section"));
+    const test = topLevelElement.querySelector('.subscription-count');
+    const count = test.innerText.substr(0, test.innerText.indexOf(' '));
+    const newCount = countUpdateFunction(parseInt(count));
+    const subscription = newCount === 1 ? 'subscription' : 'subscriptions';
+    test.innerText = `${newCount} ${subscription}`;
+  }
+
+  function subscribe(event) {
+    var input = this;
+    var form = this.form;
+
+    const XHR = new XMLHttpRequest();
+    const FD = new FormData(form);
+    const urlEncodedDataPairs = [];
+
+    for (const [name, value] of FD.entries()) {
+      urlEncodedDataPairs.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+    }
+
+    const urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+    XHR.addEventListener("load", (event) => {
+      if(XHR.status == 200) {
+        const offButton = input.id.substr(0, input.id.length-2) + 'off';
+
+        document.getElementById(input.id).checked = true;
+        document.getElementById(input.id).disabled = true;
+        document.getElementById(offButton).checked = false;
+        document.getElementById(offButton).disabled = false;
+
+        updateSubscriptionCount(form, incrementFunction);
+      }
+    });
+
+    XHR.addEventListener("error", (event) => {
+      console.log('Oops! Something went wrong.');
+    });
+
+    XHR.open("POST", form.action);
+
+    XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+    XHR.send(urlEncodedData);
   }
 
   function unsubscribe(event) {
-    const onButton = event.target.id.substr(0, event.target.id.length-3) + 'on';
-    document.getElementById(event.target.id).checked = true;
-    document.getElementById(onButton).checked = false;
-    document.getElementById(onButton).removeAttribute('checked');
+    var input = this;
+    var form = this.form;
 
-    const unsubscribeForm = event.target.parentElement;
-    unsubscribeForm.submit();
+    const XHR = new XMLHttpRequest();
+    const FD = new FormData(form);
+    const urlEncodedDataPairs = [];
+
+    for (const [name, value] of FD.entries()) {
+      urlEncodedDataPairs.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+    }
+
+    const urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+    XHR.addEventListener("load", (event) => {
+      if(XHR.status == 200) {
+        const onButton = input.id.substr(0, input.id.length-3) + 'on';
+
+        document.getElementById(input.id).checked = true;
+        document.getElementById(input.id).disabled = true;
+        document.getElementById(onButton).checked = false;
+        document.getElementById(onButton).disabled = false;
+
+        updateSubscriptionCount(form, decrementFunction);
+      }
+    });
+
+    XHR.addEventListener("error", (event) => {
+      console.log('Oops! Something went wrong.');
+    });
+
+    XHR.open("POST", form.action);
+
+    XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+    XHR.send(urlEncodedData);
   }
   
   document.querySelectorAll(".slider__on-submit").forEach(function(item, index, arr) {
