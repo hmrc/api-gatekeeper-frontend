b5b1a2ad2b1231ff11c0c2885139e722ab0b5d6d
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2026-07-14 14:58:47 +0100

    API-9306 - Updating role restrictions as per ticket

--- app/uk/gov/hmrc/gatekeeper/controllers/ApplicationController.scala
+++ app/uk/gov/hmrc/gatekeeper/controllers/ApplicationController.scala
@@ -113,7 +113,7 @@ class ApplicationController @Inject() (
     for {
       paginatedApplications <- applicationService.searchApplications(env, params)
       apis                  <- apmService.fetchNonOpenApis(env)
-    } yield Ok(applicationsView(paginatedApplications, groupApisByStatus(apis), request.role.isSuperUser, params, buildAppUrlFn))
+    } yield Ok(applicationsView(paginatedApplications, groupApisByStatus(apis), params, buildAppUrlFn))
   }
 
   def applicationsPageCsv(environment: Option[Environment] = None): Action[AnyContent] = anyAuthenticatedUserAction { implicit request =>
@@ -392,7 +392,7 @@ class ApplicationController @Inject() (
     }
   }
 
-  def manageScopes(appId: ApplicationId): Action[AnyContent] = atLeastSuperUserAction { implicit request =>
+  def manageScopes(appId: ApplicationId): Action[AnyContent] = atLeastAdvancedUserAction { implicit request =>
     withApp(appId) { app =>
       def showManageScopesView(scopes: Set[String]) =
         Future.successful(Ok(manageScopesView(app, scopesForm.fill(scopes))))
@@ -405,7 +405,7 @@ class ApplicationController @Inject() (
     }
   }
 
-  def updateScopes(appId: ApplicationId) = atLeastSuperUserAction { implicit request =>
+  def updateScopes(appId: ApplicationId) = atLeastAdvancedUserAction { implicit request =>
     withApp(appId) { app =>
       def handleValidForm(scopes: Set[String]) = {
         applicationService.updateScopes(app, scopes, loggedIn.userFullName.get).map {
@@ -515,7 +515,7 @@ class ApplicationController @Inject() (
     }
   }
 
-  def manageDeleteRestriction(appId: ApplicationId) = atLeastSuperUserAction { implicit request =>
+  def manageDeleteRestriction(appId: ApplicationId) = atLeastAdvancedUserAction { implicit request =>
     withApp(appId) { app =>
       def handleDeleteRestrictionEnabled(application: ApplicationWithCollaborators) = {
         val deleteRestriction = application.details.deleteRestriction.asInstanceOf[DoNotDelete]
@@ -537,7 +537,7 @@ class ApplicationController @Inject() (
     }
   }
 
-  def updateDeleteRestrictionPreviouslyDisabled(appId: ApplicationId): Action[AnyContent] = atLeastSuperUserAction { implicit request =>
+  def updateDeleteRestrictionPreviouslyDisabled(appId: ApplicationId): Action[AnyContent] = atLeastAdvancedUserAction { implicit request =>
     withApp(appId) { app =>
       def handleUpdateDeleteRestriction(allowDelete: Boolean, reason: String) = {
         applicationService.updateDeleteRestriction(appId, allowDelete, loggedIn.userFullName.get, reason) map { _ =>
@@ -561,7 +561,7 @@ class ApplicationController @Inject() (
     }
   }
 
-  def updateDeleteRestrictionPreviouslyEnabled(appId: ApplicationId): Action[AnyContent] = atLeastSuperUserAction { implicit request =>
+  def updateDeleteRestrictionPreviouslyEnabled(appId: ApplicationId): Action[AnyContent] = atLeastAdvancedUserAction { implicit request =>
     withApp(appId) { app =>
       def handleUpdateDeleteRestriction(allowDelete: Boolean, reason: String) = {
         applicationService.updateDeleteRestriction(appId, allowDelete, loggedIn.userFullName.get, reason) map { _ =>
@@ -707,11 +707,11 @@ class ApplicationController @Inject() (
     }
   }
 
-  def createPrivApplicationPage(): Action[AnyContent] = atLeastSuperUserAction { implicit request =>
+  def createPrivApplicationPage(): Action[AnyContent] = atLeastAdvancedUserAction { implicit request =>
     Future.successful(Ok(createApplicationView(createPrivAppForm.fill(CreatePrivAppForm()))))
   }
 
-  def createPrivApplicationAction(): Action[AnyContent] = atLeastSuperUserAction { implicit request =>
+  def createPrivApplicationAction(): Action[AnyContent] = atLeastAdvancedUserAction { implicit request =>
     def handleInvalidForm(form: Form[CreatePrivAppForm]) = {
       Future.successful(BadRequest(createApplicationView(form)))
     }
