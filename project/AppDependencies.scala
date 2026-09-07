6964b46ea0f36b64d8b54e04768f8ef4cbfdb2a9
anjumabbas5 <49648217+anjumabbas5@users.noreply.github.com>
2026-09-07 18:11:16 +0100

    APIS-8314 - Add api-platform-common-domain-fixtures to dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -35,8 +35,8 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"                    % bootstrapVersion,
     "org.jsoup"               % "jsoup"                                     % jsoupVersion,
     "uk.gov.hmrc"            %% "ui-test-runner"                            % "0.54.0",
-    "org.mockito"            %% "mockito-scala-scalatest"                   % mockitoScalaVersion,
     "org.scalacheck"         %% "scalacheck"                                % scalaCheckVersion,
+    "uk.gov.hmrc"            %% "api-platform-common-domain-fixtures"       % commonDomainVersion,
     "uk.gov.hmrc"            %% "api-platform-tpd-domain-fixtures"          % tpdDomainVersion,
     "uk.gov.hmrc"            %% "api-platform-application-domain-fixtures"  % appDomainVersion,
     "uk.gov.hmrc"            %% "api-platform-organisation-domain-fixtures" % orgDomainVersion
